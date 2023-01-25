64cce53d2660b4a4be42c8725f0422885f7040f8
Maksym Todoruk <todoruk@outlook.com>
2023-01-25 21:48:47 +0200

    HomeWork5

---
+++ src/com/ua/lesson5/Main.java
@@ -0,0 +1,27 @@
+package com.ua.lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 7, b = 18;
+        if (a > b) {
+            System.out.println(a);
+        }
+        else
+            System.out.println(b);
+
+        int c = 15, d = 9;
+        if (c < d) {
+            System.out.println(c);
+        }
+            else
+            System.out.println(d);
+
+            int x = 25;
+            if (x % 2 == 0) {
+                System.out.println("x is even");
+            }
+            else
+                System.out.println("x is not even");
+
+    }
+}
