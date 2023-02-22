26ea62b8584ae5d3d2a9f38a02aac98824dc6b9a
Maksym Todoruk <todoruk@outlook.com>
2023-02-22 21:56:53 +0200

    HomeWork12

--- src/com/ua/lesson12/Child.java
+++ src/com/ua/lesson12/Child.java
@@ -2,7 +2,7 @@ package com.ua.lesson12;
 
 public class Child extends Parent {
     static {
-        System.out.println("static Child block");
+        System.out.println("Static Child block");
     }
 
     {
@@ -13,7 +13,7 @@ public class Child extends Parent {
         System.out.println("Child block 2");
     }
     Child() {
-        System.out.println("Child constructor");
+        System.out.println("Child Constructor");
     }
 
     public static void main(String[] args) {
--- src/com/ua/lesson12/Parent.java
+++ src/com/ua/lesson12/Parent.java
@@ -2,16 +2,19 @@ package com.ua.lesson12;
 
 public class Parent {
     static {
-        System.out.println("static Parent block");
+        System.out.println("Static Parent block");
     }
+
     {
         System.out.println("Parent block 1");
     }
+
     {
         System.out.println("Parent block 2");
     }
-    Parent(){
-        System.out.println("Parent constructor");
-    }
 
+    Parent() {
+        System.out.println("Parent Constructor");
+    }
 }
+
