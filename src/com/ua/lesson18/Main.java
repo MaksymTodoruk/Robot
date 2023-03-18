d6eeb2ef434e9253c9dbb623c00d38c365cef1b5
Maksym Todoruk <todoruk@outlook.com>
2023-03-18 14:52:13 +0200

    HomeWork18

---
+++ src/com/ua/lesson18/Main.java
@@ -0,0 +1,26 @@
+package com.ua.lesson18;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SaveNumbers save = new SaveNumbers();
+        System.out.println(save.print10NumbersList());  // Save 10 random int elements invoking by the method;
+
+        List<String> a = new ArrayList<>();
+        a.add("dfgdg");
+        a.add("hjdfgsd");
+        a.add("13vnvn");
+        a.add("#ghhf");
+        a.add("Sdrrr");
+        System.out.println(a);
+        Collections.sort(a);                            // sorting with "natural order"
+        System.out.println(a);
+
+        List linked = new LinkedList();
+        for (int i = 0; i < 2000000; i++) {
+            linked.add(new Student());
+            System.out.println(linked);                 // LinkedList of 2000000 Student objects
+        }
+    }
+}
